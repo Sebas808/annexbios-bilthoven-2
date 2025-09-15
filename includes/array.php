d93275aab78fd33f79aea84e84374d790c3557b4
Sebas808 <Sebastiaan0207@gmail.com>
2025-09-15 11:59:03 +0200

    detail.php

---
+++ includes/array.php
@@ -0,0 +1,44 @@
+
+<?php
+$films = [
+  [
+    "title" => "Deadpool",
+    "poster" => "", // leeg
+    "rating" => "★★★★☆",
+    "release" => "12-02-2016",
+    "description" => "Een antiheld die zwarte humor en brute actie combineert.",
+    "actors" => [], // leeg
+    "trailer" => "https://www.youtube.com/embed/ONHBaC-pfsk"
+  ],
+  [
+    "title" => "Jurassic World: Fallen Kingdom",
+    "poster" => "", // leeg
+    "rating" => "★★★☆☆",
+    "release" => "07-06-2018",
+    "description" => "Dinosaurussen keren terug en chaos volgt op het eiland Isla Nublar.",
+    "actors" => [], // leeg
+    "trailer" => "https://www.youtube.com/embed/vn9mMeWcgoM"
+  ],
+  [
+    "title" => "Pieter Konijn",
+    "poster" => "", // leeg
+    "rating" => "★★★☆☆",
+    "release" => "22-03-2018",
+    "description" => "De ondeugende Pieter Konijn maakt de boel onveilig in de tuin van Mr. McGregor.",
+    "actors" => [], // leeg
+    "trailer" => "https://www.youtube.com/embed/7Pa_Weidt08"
+  ],
+  [
+    "title" => "Solo: A Star Wars Story",
+    "poster" => "", // leeg
+    "rating" => "★★★☆☆",
+    "release" => "23-05-2018",
+    "description" => "Het verhaal van een jonge Han Solo en zijn eerste avonturen.",
+    "actors" => [], // leeg
+    "trailer" => "https://www.youtube.com/embed/jPEYpryMp2s"
+  ]
+];
+
+// voorbeeld hoe je ze kunt tonen
+
+?>
