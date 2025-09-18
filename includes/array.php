01571cb4af55f954d94f44ee7ccea5d7391cb56c
Sebas808 <Sebastiaan0207@gmail.com>
2025-09-18 15:42:28 +0200

    detailpagina

--- includes/array.php
+++ includes/array.php
@@ -2,23 +2,27 @@
 <?php
 $films = [
   //deadpool 2:
-  [
-    "title" => "Deadpool",
-    "poster" => "assets/img/deadpool.jpg",
-    "rating" => "★★★★☆",
-    "release" => "12-02-2016",
-    "description" => '
-  <p>Een antiheld die zwarte humor en brute actie combineert.</p>
- <p style="margin:4px 0;">Genre: Actie, Komedie, Sciencefiction</p>
-<p style="margin:4px 0;">Filmlengte: 119 minuten (Theatrical) / 134 minuten (Super Duper Cut)</p>
-<p style="margin:4px 0;">Land: Verenigde Staten</p>
-<p style="margin:4px 0;">Imdb score: 7.6/10</p>
-<p style="margin:4px 0;">Regisseur: David Leitch</p>
-  <p style="margin:4px 0;">Acteurs:</p>
-',
-    "actors" => [],
-    "trailer" => "https://www.youtube.com/embed/ONHBaC-pfsk"
-  ],
+[
+  "title" => "Deadpool",
+  "poster" => "assets/img/deadpool.jpg",
+  "rating" => "★★★★☆",
+  "release" => "12-02-2016",
+  "description" => '
+    <p>Een antiheld die zwarte humor en brute actie combineert.</p>
+    <p style="margin:4px 0;">Genre: Actie, Komedie, Sciencefiction</p>
+    <p style="margin:4px 0;">Filmlengte: 119 minuten</p>
+    <p style="margin:4px 0;">Land: Verenigde Staten</p>
+    <p style="margin:4px 0;">Imdb score: 7.6/10</p>
+    <p style="margin:4px 0;">Regisseur: David Leitch</p>
+    <p style="margin:4px 0;">Acteurs:</p>
+  ',
+  "actors" => [],
+  "trailer" => "https://www.youtube.com/embed/ONHBaC-pfsk",
+  "kijkwijzer" => [
+      "assets/img/kijkwijzer-geweld.png",
+      "assets/img/kijkwijzer-eng.png"
+  ]
+],
   // jurrassic world:
   [
     "title" => "Jurassic World: Fallen Kingdom",
@@ -26,16 +30,25 @@ $films = [
     "rating" => "★★★☆☆",
     "release" => "07-06-2018",
     "description" => '
-  <p>"Dinosaurussen keren terug en chaos volgt op het eiland Isla Nublar.</p>
+  <p>"De dinosaurussen keren terug in een spannend vervolg dat begint drie jaar na de verwoesting van het themapark Jurassic World. Isla Nublar, ooit een plek vol toeristen en wonderen, is inmiddels verlaten door mensen. De dinosaurussen die daar vrij rondlopen, dreigen echter opnieuw met uitsterven wanneer een slapende vulkaan tot uitbarsting komt. De wereld staat voor een dilemma: laten we de prehistorische dieren sterven of riskeren we alles om ze te redden?.</p>
   <p style="margin:4px 0;">Genre: Actie, Avontuur, Sciencefiction</p>
 <p style="margin:4px 0;">Filmlengte: 128 minutes</p>
 <p style="margin:4px 0;">Land: USA</p>
 <p style="margin:4px 0;">Imdb score: 6.1/10</p>
 <p style="margin:4px 0;">Regisseur: J.A. Bayona</p>
-<p style="margin:4px 0;">Acteurs: Chris Pratt, Bryce Dallas Howard, Rafe Spall, Justice Smith</p>
+<p style="margin:4px 0;">Acteurs: Chris Pratt, Bryce Dallas Howard, Rafe Spall, Toby Jones</p>
 ',
-    "actors" => [],
-    "trailer" => "https://www.youtube.com/embed/vn9mMeWcgoM"
+    "actors" => [ "assets/img/Chris_pratt.jpg",
+  "assets/img/BryceDallas.jpg",
+  "assets/img/Toby_Jones.jpg",
+  "assets/img/rafe_spall.jpg"
+],
+"trailer" => "https://www.youtube.com/embed/vn9mMeWcgoM",
+"kijkwijzer" => [
+  "assets/img/kijkwijzer-geweld.png",
+  "assets/img/kijkwijzer-12.png",
+  "assets/img/kijkwijzer-eng.png"
+]
   ],
   // pieter konijn:
   [
@@ -54,6 +67,7 @@ $films = [
 ',
     "actors" => [],
     "trailer" => "https://www.youtube.com/embed/7Pa_Weidt08"
+
   ],
   // star wars solo:
   [
@@ -71,7 +85,11 @@ $films = [
   <p style="margin:4px 0;">Acteurs:</p>
 ',
     "actors" => [],
-    "trailer" => "https://www.youtube.com/embed/jPEYpryMp2s"
+    "trailer" => "https://www.youtube.com/embed/jPEYpryMp2s",
+    "kijkwijzer" => [
+    "assets/img/kijkwijzer-geweld.png",
+    "assets/img/kijkwijzer-12.png"
+]
   ]
 ];
 
